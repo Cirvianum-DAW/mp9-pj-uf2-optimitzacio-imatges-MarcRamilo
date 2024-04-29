42913f9b9c6910ad757eb51455d2f04e77eea061
MarcRamilo <marc.ramilo@crivanum.cat>
2024-04-29 19:04:45 +0200

    changes

--- src/SCRIPTS/scripts.js
+++ src/SCRIPTS/scripts.js
@@ -1,10 +1,19 @@
 
-document.addEventListener("DOMContentLoaded", function () {
-  const button = document.querySelector('[data-collapse-toggle="navbar-default"]');
-  const menu = document.getElementById('navbar-default');
 
-  button.addEventListener('click', function () {
-    menu.classList.toggle('hidden');
+document.addEventListener("DOMContentLoaded", function () {
+  const buttonNavbar = document.querySelector('[data-collapse-toggle="navbar-default"]');
+  const menuNavbar = document.getElementById('navbar-default');
+
+  buttonNavbar.addEventListener('click', function () {
+    menuNavbar.classList.toggle('hidden');
+  });
+
+  const buttonFooter = document.querySelector('[data-collapse-toggle="navbar-default-footer"]');
+  const menuFooter = document.getElementById('navbar-default-footer');
+
+  buttonFooter.addEventListener('click', function () {
+    menuFooter.classList.toggle('hidden');
   });
 });
+
 
