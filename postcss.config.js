bf5e1fa0c2241a053ad3bc6fd122d05235915731
MarcRamilo <marc.ramilo@crivanum.cat>
2024-05-07 17:19:07 +0200

    update

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,9 @@ module.exports = {
   plugins: [
     require("postcss-import"),
     require("tailwindcss"),
+    'cleanupIDs', // Place cleanupIDs before or after preset-default
+    {
+      name: 'preset-default',
+    }
   ],
 };
